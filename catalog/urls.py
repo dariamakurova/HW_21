c3d1b4636536e167e66fdff121c08442407101eb
Daria_Makurova <dariamakurova@gmail.com>
2026-03-02 23:37:34 +0300

    contacts CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from catalog import views
-from catalog.views import ProductListView, ProductDetailView
+from catalog.views import ProductListView, ProductDetailView, ContactsView
 
 app_name = 'catalog'
 
 urlpatterns = [
     path('home/', views.home, name='home'),
-    path('contacts/', views.contacts, name='contacts'),
+    path('contacts/', ContactsView.as_view(), name='contacts'),
     path('category_1/', views.category_1, name='category_1'),
     path('', ProductListView.as_view(), name='catalog'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product_info'),
--- catalog/views.py
+++ catalog/views.py
@@ -2,7 +2,7 @@ from gettext import Catalog
 
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 
 from catalog.models import Product
 
@@ -10,17 +10,17 @@ from catalog.models import Product
 def home(request):
     return render(request, 'catalog/main.html')
 
+class ContactsView(TemplateView):
+    template_name = "catalog/contacts.html"
 
-def contacts(request):
-    if request.method == 'POST':
-        # Получение данных из формы
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        message = request.POST.get('message')
-        # Обработка данных (например, сохранение в БД, отправка email и т. д.)
-        # Здесь мы просто возвращаем простой ответ
-        return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено. Мы скоро с вами свяжемся.")
-    return render(request, 'catalog/contacts.html')
+# def contacts(request):
+#     if request.method == 'POST':
+#         # Получение данных из формы
+#         name = request.POST.get('name')
+#         email = request.POST.get('email')
+#         message = request.POST.get('message')
+#         return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено. Мы скоро с вами свяжемся.")
+#     return render(request, 'catalog/contacts.html')
 
 def category_1(request):
     return render(request, 'catalog/category_1.html')
