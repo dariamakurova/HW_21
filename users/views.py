e72b59c7979a706ad71a28da31ee46d1dac7c96e
Daria_Makurova <dariamakurova@gmail.com>
2026-04-01 01:09:48 +0300

    access control

--- users/views.py
+++ users/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy, reverse
 from django.views.generic import DetailView
 from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from config.settings import EMAIL_HOST_USER
 from .forms import UserRegisterForm, UserForm
